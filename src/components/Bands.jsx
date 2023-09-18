42183f410b0b6c4e222eaeb58093315b44207d5e
Sebastian Camilo Ramirez Betancur <sebastianramirez748@gmail.com>
2023-09-18 18:29:25 -0500

    fixed margin

--- src/components/Bands.jsx
+++ src/components/Bands.jsx
@@ -60,7 +60,7 @@ export const Bands = () => {
     return (
         <section className=" w-full h-screen flex flex-col items-center justify-center pt-20 max-[800px]:pt-24 px-14">
             <div>
-                <h2 className="text-center text-4xl mt-20 mb-20 max-[800px]:mb-4">bandas</h2>
+                <h2 className="text-center text-4xl mt-16 mb-20 max-[800px]:mb-4">bandas</h2>
                 <p className="uppercase text-center text-[#FF8418] ">
                     <div className="mb-4 text-7xl max-[800px]:text-base flex flex-wrap justify-center gap-4 text-center [&>span]:text-white">
                         <span className="text-7xl max-[800px]:text-2xl uppercase">rocka</span> | <span className="text-7xl max-[800px]:text-2xl uppercase">stayway</span>
