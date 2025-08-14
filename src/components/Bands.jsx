5d9ff829f2524928ea0188364e9327c215fef45f
Sebastian Ramirez <sebastianramirez748@gmail.com>
2025-08-14 14:14:45 -0500

    refactor: update band list in Bands component and remove unused variables

--- src/components/Bands.jsx
+++ src/components/Bands.jsx
@@ -1,59 +1,5 @@
 import React from 'react'
 
-const musicBandsL = [
-    {
-        name: 'rocka'
-    },
-    {
-        name: 'stayway'
-    },
-];
-
-const musicBandsM = [
-    {
-        name: 'moth'
-    },
-    {
-        name: 'paris sobre llamas'
-    },
-    {
-        name: 'nauj project'
-    }
-];
-
-const musicBandsS = [
-    {
-        name: 'silversmoke'
-    },
-    {
-        name: 'sunset street'
-    },
-    {
-        name: 'job'
-    },
-    {
-        name: 'rokamandú'
-    },
-    {
-        name: 'santa pacha'
-    },
-    {
-        name: 'constelan sound'
-    },
-    {
-        name: 'paradoja'
-    },
-    {
-        name: 'sonic barton'
-    },
-    {
-        name: 'riverstone'
-    }
-
-];
-
-
-
 
 
 export const Bands = () => {
@@ -63,18 +9,18 @@ export const Bands = () => {
                 <h2 className="text-center text-4xl mt-16 mb-20 max-[800px]:mb-4">bandas</h2>
                 <div className="uppercase text-center text-[#FF8418] ">
                     <div className="mb-4 text-7xl max-[800px]:text-base flex flex-wrap justify-center gap-4 text-center [&>span]:text-white">
-                        <span className="text-7xl max-[800px]:text-2xl uppercase">rocka</span> | <span className="text-7xl max-[800px]:text-2xl uppercase">stayway</span>
+                        <span className="text-7xl max-[800px]:text-2xl uppercase">pez errante</span> 
                     </div>
                     <div className="mb-4 text-5xl max-[800px]:text-lg flex flex-wrap justify-center gap-4 text-center [&>span]:text-white">
-                        <span className="text-5xl max-[800px]:text-xl uppercase">moth</span> | <span className="text-5xl max-[800px]:text-xl uppercase">paris sobre llamas</span> | <span className="text-5xl max-[800px]:text-xl uppercase">nauj project</span>
+                        <span className="text-5xl max-[800px]:text-xl uppercase">autopista</span> | <span className="text-5xl max-[800px]:text-xl uppercase">nauj project</span>
                     </div>
                     <div className="mb-2 text-4xl max-[800px]:text-base flex flex-wrap justify-center gap-4 text-center [&>span]:text-white">
-                        <span className="text-4xl max-[800px]:text-lg uppercase">silversmoke</span> | <span className="text-4xl max-[800px]:text-lg uppercase">sunset street</span> | <span className="text-4xl max-[800px]:text-lg uppercase">job</span> | <span
-                                className="text-4xl max-[800px]:text-lg uppercase">santa pacha</span> | <span
-                                    className="text-4xl max-[800px]:text-lg uppercase">constelan sound</span> | <span
-                                        className="text-4xl max-[800px]:text-lg uppercase">electrica</span> | <span
-                                            className="text-4xl max-[800px]:text-lg uppercase">sonic barton</span> | <span
-                                                className="text-4xl max-[800px]:text-lg uppercase">riverstone</span>
+                         <span
+                            className="text-4xl max-[800px]:text-lg uppercase">santa pacha</span> | <span
+                                className="text-4xl max-[800px]:text-lg uppercase">himera</span> | <span
+                                    className="text-4xl max-[800px]:text-lg uppercase">jack n`grass</span> | <span
+                                        className="text-4xl max-[800px]:text-lg uppercase">lékel</span> | <span
+                                            className="text-4xl max-[800px]:text-lg uppercase">justine y el infortunio</span>
                     </div>
                 </div>
             </div>
