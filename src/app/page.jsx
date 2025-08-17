e1fe62ac562abc0d3c3ee69e5007f4169b0d17f6
Sebastian Ramirez <sebastianramirez748@gmail.com>
2025-08-17 10:17:37 -0500

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -30,7 +30,7 @@ export default function Home() {
 
       <Script
         src="https://rockbot-924631262984.southamerica-west1.run.app/widget.js"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
     </>
   );
