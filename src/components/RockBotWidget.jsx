7b8ea341573f5bd48a58f5e540806dd6bfd55904
Sebastian Ramirez <sebastianramirez748@gmail.com>
2025-08-16 22:06:32 -0500

    refactor: change script loading strategy to lazyOnload in RockBotWidget

--- src/components/RockBotWidget.jsx
+++ src/components/RockBotWidget.jsx
@@ -19,7 +19,8 @@ export default function RockBotWidget() {
       <div id="chat-widget"></div> 
       <Script
         src="https://rockbot-924631262984.southamerica-west1.run.app/widget.js"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
+        
       />
     </>
   );
