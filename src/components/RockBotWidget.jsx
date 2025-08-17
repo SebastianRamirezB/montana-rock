95a6c469d6e2cca86b5f180e297dec205e74fa83
Sebastian Ramirez <sebastianramirez748@gmail.com>
2025-08-16 21:58:22 -0500

    refactor: remove redundant chat widget div creation from return statement

--- src/components/RockBotWidget.jsx
+++ src/components/RockBotWidget.jsx
@@ -15,7 +15,7 @@ export default function RockBotWidget() {
 
   return (
     <>
-      
+      <div id="chat-widget"></div>
       <Script
         src="https://rockbot-924631262984.southamerica-west1.run.app/widget.js"
         strategy="afterInteractive"
