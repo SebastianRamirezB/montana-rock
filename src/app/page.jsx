9d81b6dbbcdec01a5e6ba939cbc54d28941f5203
Sebastian Ramirez <sebastianramirez748@gmail.com>
2025-08-16 22:34:41 -0500

    refactor: remove unused layout elements from Home component in page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,25 +12,8 @@ import { Ticket } from '@/components/Ticket'
 export default function Home() {
   return (
     <>
-      <main className="snap-y snap-mandatory relative w-full h-screen overflow-y-auto scroll-smooth">
-        <Header />
-      <div className="snap-center" >
-        <Ticket />
-      </div>
-      <div className="snap-center">
-        <Bands />
-      </div>
-      {/* <div className="snap-center">
-        <Sponsors />
-      </div> */}
-      <div className="snap-center">
-        <Playlist/>
-      </div>
-      <div className="snap-center max-[800px]:hidden">
-        <Gallery />
-      </div>
+     
       <RockBotWidget />
-      </main>
    </>
   )
 }
