a2d199618e73123927846b95c09fe1170c0e376b
Sebastian Ramirez <sebastianramirez748@gmail.com>
2025-08-16 22:36:03 -0500

    refactor: clean up layout structure in Home component of page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,24 @@ import { Ticket } from '@/components/Ticket'
 export default function Home() {
   return (
     <>
-     
+      <main className="snap-y snap-mandatory relative w-full h-screen overflow-y-auto scroll-smooth">
+        <Header />
+      <div className="snap-center" >
+        <Ticket />
+      </div>
+      <div className="snap-center">
+        <Bands />
+      </div>
+      {/* <div className="snap-center">
+        <Sponsors />
+      </div> */}
+      <div className="snap-center">
+        <Playlist/>
+      </div>
+      <div className="snap-center max-[800px]:hidden">
+        <Gallery />
+      </div>
+      </main>
       <RockBotWidget />
    </>
   )
